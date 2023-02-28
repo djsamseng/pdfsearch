9bc2d641b1801b7fa504ff2cad7fdd954104394d
Samuel Seng <djsamseng@gmail.com>
2023-02-28 15:54:25 -0500

    Fix identify circles. Uses priority queue DFS finding cycles within the DAG

---
+++ lambdacontainer/processpdffunction/pdfextract/linejoiner.py
@@ -0,0 +1,122 @@
+
+import collections
+import math
+import typing
+
+from . import path_utils
+
+def check_circle_angles_from_ordered_lines(
+  ordered_lines: typing.List[path_utils.LinePointsType],
+):
+  cur_angle = path_utils.line_angle(line=ordered_lines[0])
+  for next_line in ordered_lines:
+    next_angle = path_utils.line_angle(line=next_line)
+    angle_diff = path_utils.get_angle_diff(a=cur_angle, b=next_angle, directional=False)
+    if angle_diff > 20:
+      return False
+
+    cur_angle = next_angle
+  return True
+
+def extract_cycle_from_ordered_dict(
+  cycle_start_line: path_utils.LinePointsType,
+  ordered_dict: typing.OrderedDict[
+    path_utils.LinePointsType,
+    bool
+  ],
+):
+  out: typing.List[path_utils.LinePointsType] = []
+  for line in ordered_dict.keys():
+    if len(out) > 0:
+      out.append(line)
+    elif line == cycle_start_line:
+      out.append(line)
+  return out
+
+def find_cyclical_within(
+  start_line: path_utils.LinePointsType,
+  point_to_lines: typing.DefaultDict[
+    typing.Tuple[float, float],
+    typing.List[path_utils.LinePointsType]
+  ],
+  lines_traversed: typing.Set[path_utils.LinePointsType],
+):
+  # Priority queue, DFS, explore all possibilities from a line
+  q: typing.List[
+    typing.OrderedDict[
+      path_utils.LinePointsType,
+      bool,
+    ]
+  ] = [collections.OrderedDict()]
+  q[0][start_line] = True
+  lines_traversed.add(start_line)
+  circles: typing.List[typing.List[path_utils.LinePointsType]] = []
+  while len(q) > 0:
+    lines_so_far = q.pop()
+    cur_line = next(reversed(lines_so_far))
+    use_end = lines_so_far[cur_line]
+    if use_end:
+      _, _, x, y = cur_line
+    else:
+      x, y, _, _ = cur_line
+    next_lines = point_to_lines[(x, y)]
+    for next_line in next_lines:
+      if next_line != cur_line and next_line in lines_so_far:
+        circle = extract_cycle_from_ordered_dict(
+          cycle_start_line=next_line,
+          ordered_dict=lines_so_far,
+        )
+        if len(circle) > 5:
+          if check_circle_angles_from_ordered_lines(ordered_lines=circle):
+            circles.append(circle)
+      elif next_line not in lines_traversed:
+        lines_traversed.add(next_line)
+        new_dict: typing.OrderedDict[
+          path_utils.LinePointsType,
+          bool,
+        ] = collections.OrderedDict()
+        for line_so_far, use_end in lines_so_far.items():
+          new_dict[line_so_far] = use_end
+        if (next_line[0], next_line[1]) == (x, y):
+          new_dict[next_line] = True
+        else:
+          new_dict[next_line] = False
+        q.append(new_dict)
+
+  return circles
+
+def identify_circle(
+  lines: typing.List[path_utils.LinePointsType]
+):
+  point_to_lines: typing.DefaultDict[
+    typing.Tuple[float, float],
+    typing.List[path_utils.LinePointsType]
+  ] = collections.defaultdict(list)
+  if len(lines) < 5:
+    return False
+  for line in lines:
+    x0, y0, x1, y1 = line
+    point_to_lines[(x0, y0)].append(line)
+    point_to_lines[(x1, y1)].append(line)
+
+  lines_set = set(lines)
+  lines_traversed: typing.Set[path_utils.LinePointsType] = set()
+  remaining = lines_set.difference(lines_traversed)
+  all_circles: typing.List[typing.List[path_utils.LinePointsType]] = []
+  while len(remaining) > 0:
+    start_line = remaining.pop()
+    lines_traversed.add(start_line)
+    circles = find_cyclical_within(
+      start_line=start_line,
+      point_to_lines=point_to_lines,
+      lines_traversed=lines_traversed,
+    )
+    all_circles.extend(circles)
+
+  if len(all_circles) > 0:
+    return True
+  return False
+
+
+def match_table():
+  return False
